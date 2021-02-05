c11d1d9174643873662519d7ab0f7082d47c00c7
jaeho3 <hojoh@sheridancollege.ca>
2021-02-05 12:37:29 -0500

    Created object c2 - 7 Hearts

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -8,11 +8,16 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 public class CardTrick {
     public static void main(String[] args)
     {
-       Card c1 = new Card (); 
+       Card c1 = new Card(); 
        c1.setSuit("Spades"); 
        c1.setValue(10);
         
-        System.out.println(c1); 
+       System.out.println(c1); 
+       
+       Card c2 = new Card(); 
+       c2.setSuit("Hearts");
+       c2.getValue(7); 
+        
     }
     
 }
