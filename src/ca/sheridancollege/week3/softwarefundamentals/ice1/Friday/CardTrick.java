9ffe5784ba97fc4344e7fba0cdd9d8ac8f79bafb
jaeho3 <hojoh@sheridancollege.ca>
2021-02-05 12:25:00 -0500

    Created object c1 - 10 spades

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,12 +3,14 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author John Ho - 991619682
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card c1 = new Card (); 
+       c1.setSuit("Spades"); 
+       c1.setValue(10);
     }
     
 }
